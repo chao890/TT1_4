c79f86b6379aa565951df3266fd16f807ae9d6fe
azmil4901 <azmil4901@gmail.com>
2025-01-11 12:30:16 +0800

    adding currrent work

--- backend/users/userController.js
+++ backend/users/userController.js
@@ -1,29 +1,39 @@
-const userService = require("./userService")
+const userService = require("./userService");
 
 module.exports = {
-    signin: async (req, res) => {
-        const [valid, token] = await userService.singin(req)
-        if (valid) {
-            res.cookie("token", token, { httpOnly: true })
-            return res.status(200).send("signin successful")
-        } else {
-            return res.status(401).send("Invalid username or password")
-        }
-    },
-
-    signup: (req, res) => {
-        userService.signup(req)
-            .then(result => res.status(200).send("Signup success"))
-            .catch(error => {
-                if (error.code == "ER_DUP_ENTRY") {
-                    res.status(422).send("Username is already taken")
-                } else {
-                    res.status(500).send("Internal server error")
-                }
-            })
-    },
-
-    temp: (req, res) => {
-        res.status(200).send("testing")
+  signin: async (req, res) => {
+    const [valid, token] = await userService.singin(req);
+    if (valid) {
+      res.cookie("token", token, { httpOnly: true });
+      return res.status(200).send("signin successful");
+    } else {
+      return res.status(401).send("Invalid username or password");
     }
-}
+  },
+
+  signup: (req, res) => {
+    userService
+      .signup(req)
+      .then((result) => res.status(200).send("Signup success"))
+      .catch((error) => {
+        if (error.code == "ER_DUP_ENTRY") {
+          res.status(422).send("Username is already taken");
+        } else {
+          res.status(500).send("Internal server error");
+        }
+      });
+  },
+
+  getAllRequests: async (req, res) => {
+    try {
+      const requests = await requestService.getAllRequests(req.query);
+      res.json({ requests });
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  },
+
+  temp: (req, res) => {
+    res.status(200).send("testing");
+  },
+};

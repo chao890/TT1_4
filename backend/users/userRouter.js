e6ed4a184499a60f984820889c3c1a963d0b38d3
bojie3 <xubojie2013@gmail.com>
2025-01-11 12:08:54 +0800

    Add gitignore file

--- backend/users/userRouter.js
+++ backend/users/userRouter.js
@@ -4,7 +4,6 @@ const userController = require('./userController')
 const { authenticate } = require('./userService')
 
 router.post("/users/login", userController.login)
-router.post("/users/signup", userController.signup)
 router.get("/users/info", authenticate, userController.temp)
 
 module.exports = router

de00e05901387c4b2337ce61d3d324629ca9ea2b
azmil4901 <azmil4901@gmail.com>
2025-01-11 15:14:04 +0800

    Resolve merge conflicts

--- backend/users/userRouter.js
+++ backend/users/userRouter.js
@@ -3,8 +3,6 @@ const router = express.Router();
 const userController = require("./userController");
 const { authenticate } = require("./userService");
 
-router.post("/users/signin", userController.signin);
-router.post("/users/signup", userController.signup);
 router.get("/users/info", authenticate, userController.temp);
 router.get("/users/requests", userController.getAllRequests);
 router.delete("/:id", userController.deleteRequest);

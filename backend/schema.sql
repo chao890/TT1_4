582fa2ceeab4954dd447d432161c0f7fb71859fb
bojie3 <xubojie2013@gmail.com>
2025-01-11 11:44:09 +0800

    Update schema.sql file for database format

--- backend/schema.sql
+++ backend/schema.sql
@@ -1,13 +1,34 @@
 CREATE DATABASE hackathon;
 USE hackathon;
 
-DROP TABLE IF EXISTS users;
 CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
+    username VARCHAR(255) NOT NULL,
     password TEXT NOT NULL,
-    UNIQUE (name)
+    companyName TEXT NOT NULL,
+    UNIQUE (username)
 );
 
-INSERT INTO users (name, password)
-VALUES ('admin', 'admin');
+CREATE TABLE company (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    companyName VARCHAR(255) NOT NULL,
+    activeAccount TEXT NOT NULL,
+    carbonBalance FLOAT,
+    cashBalance FLOAT,
+    createdDatetime TIMESTAMP,
+    updatedDatetime TIMESTAMP,
+    UNIQUE (companyName)
+);
+
+CREATE TABLE requests (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    companyId INT NOT NULL,
+    requestorCompanyId INT NOT NULL,
+    carbonUnitPrice FLOAT,
+    carbonQuantity FLOAT,
+    requestReason TEXT,
+    requestStatus VARCHAR(255),
+    requestType VARCHAR(255),
+    createdDatetime TIMESTAMP,
+    updatedDatetime TIMESTAMP
+);

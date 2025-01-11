39046d4f2e77b691aea1009efbc6035fa13ff2d2
azmil4901 <azmil4901@gmail.com>
2025-01-11 14:53:56 +0800

    delete and get request list functions
    delete and get request list functions

--- backend/users/userRouter.js
+++ backend/users/userRouter.js
@@ -1,9 +1,12 @@
-const express = require('express')
-const router = express.Router()
-const userController = require('./userController')
-const { authenticate } = require('./userService')
+const express = require("express");
+const router = express.Router();
+const userController = require("./userController");
+const { authenticate } = require("./userService");
 
-router.post("/users/login", userController.login)
-router.get("/users/info", authenticate, userController.temp)
+router.post("/users/signin", userController.signin);
+router.post("/users/signup", userController.signup);
+router.get("/users/info", authenticate, userController.temp);
+router.get("/users/requests", requestController.getRequests);
+router.delete("/:id", requestController.deleteRequest);
 
-module.exports = router
+module.exports = router;
--- backend/users/userService.js
+++ backend/users/userService.js
@@ -1,32 +1,62 @@
-const pool = require("../database/database")
-const bcrypt = require("bcrypt")
-const jwt = require('jsonwebtoken')
-const { SECRET_KEY } = require('../config/config')
+const pool = require("../database/database");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const { SECRET_KEY } = require("../config/config");
+//require db
+const db = require("../database/database.js");
 
 module.exports = {
-    login: async (req) => {
-        const username = req.body.username
-        const [row] = await pool.query("SELECT password FROM users WHERE name=?", [username])
-        if (!row.length || !row[0]["password"]) {
-            return [false, null]
-        }
-        valid = await bcrypt.compare(req.body.password, row[0]["password"])
-        if (!valid) {
-            return [false, null]
-        }
-        const token = jwt.sign({ username }, SECRET_KEY, { expiresIn: "1h" });
-        return [true, token]
-    },
-
-    authenticate: (req, res, next) => {
-        const token = req.cookies.token
-        try {
-            const user = jwt.verify(token, SECRET_KEY)
-            req.user = user
-            next()
-        } catch (err) {
-            res.clearCookie("token")
-            return res.status(401).json({ redirect: "/temp" });
-        }
+  login: async (req) => {
+    const username = req.body.username;
+    const [row] = await pool.query("SELECT password FROM users WHERE name=?", [
+      username,
+    ]);
+    if (!row.length || !row[0]["password"]) {
+      return [false, null];
     }
-}
+    valid = await bcrypt.compare(req.body.password, row[0]["password"]);
+    if (!valid) {
+      return [false, null];
+    }
+    const token = jwt.sign({ username }, SECRET_KEY, { expiresIn: "1h" });
+    return [true, token];
+  },
+
+  authenticate: (req, res, next) => {
+    const token = req.cookies.token;
+    try {
+      const user = jwt.verify(token, SECRET_KEY);
+      req.user = user;
+      next();
+    } catch (err) {
+      res.clearCookie("token");
+      return res.status(401).json({ redirect: "/temp" });
+    }
+  },
+
+  getAllRequests: () => {
+    return new Promise((resolve, reject) => {
+      const query = "SELECT * FROM Requests";
+      db.query(query, (err, results) => {
+        if (err) return reject(err);
+        resolve(results);
+      });
+    });
+  },
+
+  // Delete a specific request by ID
+  deleteRequest: (requestId) => {
+    return new Promise((resolve, reject) => {
+      const query = "DELETE FROM Requests WHERE id = ?";
+      db.query(query, [requestId], (err, results) => {
+        if (err) return reject(err);
+        resolve(results);
+      });
+    });
+  },
+};
+
+// module.exports = {
+//     getAllRequests,
+//     deleteRequest,
+// };
